a8580bd444b15f670af212feed5231bc10c268ce
Luke <lucian.io@outlook.com>
2024-05-02 19:55:13 -0400

    first commit

---
+++ config/site.ts
@@ -0,0 +1,34 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+	name: "Next.js + NextUI",
+	description: "Make beautiful websites regardless of your design experience.",
+	navItems: [
+		{
+			label: "Home",
+			href: "/",
+		},
+		{
+			label: "About",
+			href: "/about",
+		},
+		{
+			label: "Projects",
+			href: "/docs",
+		},
+		{
+			label: "Experience",
+			href: "/pricing",
+		},
+		{
+			label: "Contact",
+			href: "/blog",
+		},
+	],
+	links: {
+		linkedin: "https://www.linkedin.com/in/lukeanthony007/",
+		github: "https://github.com/ln64-git",
+		twitter: "https://twitter.com/OrpheusTwin",
+		discord: "https://discord.com/users/354823920010002432",
+	},
+};
