2cbb0d247da2aa96c1fc8cfff19b458405f558d1
Luke <lucian.io@outlook.com>
2024-05-04 04:58:06 -0400

    added text-generate-effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Link } from "@nextui-org/link";
 import { Snippet } from "@nextui-org/snippet";
 import { Code } from "@nextui-org/code";
@@ -6,29 +7,46 @@ import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 import { AuroraBackground } from "@/components/aurora-background";
-
+import { TextGenerateEffect } from "@/components/text-generate-effect";
+import { motion } from "framer-motion";
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-2xl text-center justify-center">
-        {/* <h1 className={title()}>Make&nbsp;</h1> */}
-        <h1 className={title({ color: "violet" })}>Innovative&nbsp;</h1>
+        {/* <h1 className={title({ color: "violet" })}>Innovative&nbsp;</h1>
         <br />
         <h1 className={title()}>software developer with experience in</h1>
         <h1 className={title({ color: "blue" })}> Cybersecurity, </h1>
         <h1 className={title()}>and </h1>
         <h1 className={title({ color: "yellow" })}> Full-Stack </h1>
-        <h1 className={title()}>Develpment.</h1>
+        <h1 className={title()}>Develpment.</h1> */}
 
+        <TextGenerateEffect
+          className={title()}
+          words={
+            "Innovative software developer with experience in Cybersecurity, and Full-Stack Develpment."
+          }
+        />
         {/* <h1 className={title({ color: "cyan" })}> frontend, </h1> */}
         {/* <h1 className={title({ color: "yellow" })}> backend development.</h1> */}
-        <h2 className={subtitle({ class: "mt-4" })}>
-          Beautiful, efficient and accessible applications.
-        </h2>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 3.5, duration: 1 }}
+        >
+          <h2 className={subtitle({ class: "mt-4" })}>
+            Beautiful, efficient and accessible applications.
+          </h2>
+        </motion.div>
       </div>
 
-      <div className="flex gap-3">
-        <Link
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 4, duration: 1 }}
+        className="flex gap-3"
+      >
+        {/* <Link
           isExternal
           href={siteConfig.links.discord}
           className={buttonStyles({
@@ -38,7 +56,7 @@ export default function Home() {
           })}
         >
           Chat with me!
-        </Link>
+        </Link> */}
         <Link
           isExternal
           className={buttonStyles({ variant: "bordered", radius: "full" })}
@@ -47,7 +65,7 @@ export default function Home() {
           <GithubIcon size={20} />
           GitHub
         </Link>
-      </div>
+      </motion.div>
     </section>
   );
 }
---
+++ components/text-generate-effect.tsx
@@ -0,0 +1,78 @@
+"use client";
+import { useEffect } from "react";
+import { motion, stagger, useAnimate } from "framer-motion";
+import { cn } from "@/utils/cn";
+import { title } from "@/components/primitives";
+
+export const TextGenerateEffect = ({
+  words,
+  className,
+}: {
+  words: string;
+  className?: string;
+}) => {
+  const [scope, animate] = useAnimate();
+  let wordsArray = words.split(" ");
+
+  useEffect(() => {
+    animate(
+      "span",
+      {
+        opacity: 1,
+      },
+      {
+        duration: 2,
+        delay: stagger(0.2),
+      }
+    );
+  }, [scope.current]);
+
+  const actionWordOneClasses = cn(title({ color: "violet" }), "opacity-0");
+  const actionWordTwoClasses = cn(title({ color: "blue" }), "opacity-0");
+  const actionWordThreeClasses = cn(title({ color: "yellow" }), "opacity-0");
+
+  const wordClasses = (index: number) => {
+    if (index === 0) {
+      return actionWordOneClasses;
+    } else if (index === 6) {
+      return actionWordTwoClasses;
+    } else if (index === 8 || index === 9) {
+      return actionWordThreeClasses;
+    } else {
+      return cn(title(), "opacity-0");
+    }
+  };
+
+  const renderWords = () => {
+    return (
+      <motion.div ref={scope}>
+        {wordsArray.map((word, index) => {
+          if (index == 0 || index == 3 || index == 6) {
+            return (
+              <motion.span key={word + index} className={wordClasses(index)}>
+                {word + " "}
+                <br />
+              </motion.span>
+            );
+          } else {
+            return (
+              <motion.span key={word + index} className={wordClasses(index)}>
+                {word + " "}
+              </motion.span>
+            );
+          }
+        })}
+      </motion.div>
+    );
+  };
+
+  return (
+    <div className={cn("font-bold", className)}>
+      <div className="mt-4">
+        <div className=" dark:text-white text-black text-2xl leading-snug tracking-wide">
+          {renderWords()}
+        </div>
+      </div>
+    </div>
+  );
+};
