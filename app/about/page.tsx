86257d6aa7b482051837cacf9b64289fcb248960
Luke <lucian.io@outlook.com>
2024-05-04 06:54:57 -0400

    added background-gradient

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,24 +1,34 @@
+import { BackgroundGradient } from "@/components/background-gradient";
 import { title } from "@/components/primitives";
-import { Card, CardBody, Divider, Image, ScrollShadow } from "@nextui-org/react";
+import {
+  Card,
+  CardBody,
+  Divider,
+  Image,
+  ScrollShadow,
+} from "@nextui-org/react";
 
 export default function AboutPage() {
   return (
-    <>
-		    <ScrollShadow className="w-[300px] h-[400px]">
-      <Card>
-        <CardBody>
-          <div>
-            <Image
-              width={300}
-              alt="NextUI hero Image"
-              src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
-            />
-            <h1 className={title()}>About</h1>
-          </div>
-        </CardBody>
-      </Card>
-      <Divider className="my-4" />
-			</ScrollShadow>
-    </>
+    <div className="w-full flex flex-col justify-center ">
+      <div className="max-w-lg mx-auto">
+        <BackgroundGradient>
+          <Card>
+            <CardBody>
+              <div>
+                <Image
+                  width={300}
+                  alt="NextUI hero Image"
+                  src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
+                />
+                <h1 className={title()}>About</h1>
+              </div>
+            </CardBody>
+          </Card>
+        </BackgroundGradient>
+      </div>
+
+      {/* <Divider className="my-4" /> */}
+    </div>
   );
 }
