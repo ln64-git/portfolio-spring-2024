c7cf0b5921ff9a2f65048a95ce51797073533686
Luke <lucian.io@outlook.com>
2024-05-05 17:26:10 -0400

    changed format of about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -37,133 +37,166 @@ export default function AboutPage() {
           />
         </div>
         <div className=" flex items-center justify-center">
-          <div className="ml-4 flex flex-col items-center ">
-            <h1 className="inline font-semibold text-2xl md:text-4xl text-left  w-full px-4 md:px-0">
-              About
-            </h1>
-            <div className="leading-5 px-auto mx-auto max-w-[400px] text-sm px-4 md:px-0 sm:text-lg items-center text-left justify-center">
-              {summary}
+          <div className="ml-2 flex flex-col items-center ">
+            <div className="inline text-2xl md:text-5xl text-left pb-1 w-full px-4 md:px-0">
+              {name}
+            </div>
+
+            <div className="leading-6 md:leading-7 px-auto mx-auto max-w-[400px] text-sm px-4 md:px-0 sm:text-lg text-left justify-center">
+              <ul>
+                {info.map((element, index) => (
+                  <li key={index} className="text-xl my-2  font-light text-left">
+                    {element}
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
         </div>
       </div>
-      <div className="px-4 mx-auto w-full flex max-w-[800px] items-center justify-around ">
-        <p className="px-auto mx-auto pt-2 text-left text-md sm:text-xl items-center justify-center leading-6">
-          I am 27 years old and eager to prove myself in the field of
-          Technology. As someone who has been passionate about computers since I
-          was a young I decided 3 years ago that I was ready to apply myself to
-          the deep study of computer systems and application development. In
-          this time have attended a full-stack web development bootcamp
-          specializing in Java and acquired certification in cybersecurity.
-          Outside of my studies I built multiple full-stack application using
-          JavaScript meta-frameworks such as Next.js, and Nuxt.js, I am
-          currently expanding my knowledge of systems level programming with
-          Rust applications while utilizing Microsoft Azure Web Services . I
-          have a passion for building mobile, desktop and web applications which
-          focus on delivering high levels of efficiency, utility and
-          accessibility.
-        </p>
+
+      <div className="pt-10">
+        <div className="px-4 pb-4 mx-auto w-full max-w-[800px] items-center justify-around flex flex-col">
+          <h1 className="max-w-[700px] inline font-semibold text-2xl md:text-3xl text-left  w-full px-4 md:px-0">
+            Front-End
+          </h1>
+
+          <p className="leading-7 md:leading-9 px-auto mx-auto pt-2 text-left text-md sm:text-xl items-center justify-center max-w-[700px] ">
+            I built multiple full-stack application using JavaScript
+            meta-frameworks such as Next.js, and Nuxt.js, I am currently
+            expanding
+          </p>
+        </div>
+        <div className="pt-4 max-w-[700px] mx-auto">
+          <Accordion variant="splitted">
+            <AccordionItem key="1" aria-label="Skills" title="Skills">
+              <Table aria-label="Example static collection table">
+                <TableHeader>
+                  <TableColumn>NAME</TableColumn>
+                  <TableColumn>ROLE</TableColumn>
+                  <TableColumn>STATUS</TableColumn>
+                </TableHeader>
+                <TableBody>
+                  <TableRow key="1">
+                    <TableCell>Tony Reichert</TableCell>
+                    <TableCell>CEO</TableCell>
+                    <TableCell>Active</TableCell>
+                  </TableRow>
+                  <TableRow key="2">
+                    <TableCell>Zoey Lang</TableCell>
+                    <TableCell>Technical Lead</TableCell>
+                    <TableCell>Paused</TableCell>
+                  </TableRow>
+                  <TableRow key="3">
+                    <TableCell>Jane Fisher</TableCell>
+                    <TableCell>Senior Developer</TableCell>
+                    <TableCell>Active</TableCell>
+                  </TableRow>
+                  <TableRow key="4">
+                    <TableCell>William Howard</TableCell>
+                    <TableCell>Community Manager</TableCell>
+                    <TableCell>Vacation</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </AccordionItem>
+          </Accordion>
+        </div>
       </div>
-      <div className="pt-4 max-w-[700px] mx-auto">
-        <Accordion variant="splitted">
-          <AccordionItem key="1" aria-label="Front-End" title="Front-End">
-            <Table aria-label="Example static collection table">
-              <TableHeader>
-                <TableColumn>NAME</TableColumn>
-                <TableColumn>ROLE</TableColumn>
-                <TableColumn>STATUS</TableColumn>
-              </TableHeader>
-              <TableBody>
-                <TableRow key="1">
-                  <TableCell>Tony Reichert</TableCell>
-                  <TableCell>CEO</TableCell>
-                  <TableCell>Active</TableCell>
-                </TableRow>
-                <TableRow key="2">
-                  <TableCell>Zoey Lang</TableCell>
-                  <TableCell>Technical Lead</TableCell>
-                  <TableCell>Paused</TableCell>
-                </TableRow>
-                <TableRow key="3">
-                  <TableCell>Jane Fisher</TableCell>
-                  <TableCell>Senior Developer</TableCell>
-                  <TableCell>Active</TableCell>
-                </TableRow>
-                <TableRow key="4">
-                  <TableCell>William Howard</TableCell>
-                  <TableCell>Community Manager</TableCell>
-                  <TableCell>Vacation</TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </AccordionItem>
-          <AccordionItem key="2" aria-label="Back-End" title="Back-End">
-            <Table aria-label="Example static collection table">
-              <TableHeader>
-                <TableColumn>NAME</TableColumn>
-                <TableColumn>ROLE</TableColumn>
-                <TableColumn>STATUS</TableColumn>
-              </TableHeader>
-              <TableBody>
-                <TableRow key="1">
-                  <TableCell>Tony Reichert</TableCell>
-                  <TableCell>CEO</TableCell>
-                  <TableCell>Active</TableCell>
-                </TableRow>
-                <TableRow key="2">
-                  <TableCell>Zoey Lang</TableCell>
-                  <TableCell>Technical Lead</TableCell>
-                  <TableCell>Paused</TableCell>
-                </TableRow>
-                <TableRow key="3">
-                  <TableCell>Jane Fisher</TableCell>
-                  <TableCell>Senior Developer</TableCell>
-                  <TableCell>Active</TableCell>
-                </TableRow>
-                <TableRow key="4">
-                  <TableCell>William Howard</TableCell>
-                  <TableCell>Community Manager</TableCell>
-                  <TableCell>Vacation</TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </AccordionItem>
-          <AccordionItem
-            key="3"
-            aria-label="Cybersecurity"
-            title="Cybersecurity"
-          >
-            <Table aria-label="Example static collection table">
-              <TableHeader>
-                <TableColumn>NAME</TableColumn>
-                <TableColumn>ROLE</TableColumn>
-                <TableColumn>STATUS</TableColumn>
-              </TableHeader>
-              <TableBody>
-                <TableRow key="1">
-                  <TableCell>Tony Reichert</TableCell>
-                  <TableCell>CEO</TableCell>
-                  <TableCell>Active</TableCell>
-                </TableRow>
-                <TableRow key="2">
-                  <TableCell>Zoey Lang</TableCell>
-                  <TableCell>Technical Lead</TableCell>
-                  <TableCell>Paused</TableCell>
-                </TableRow>
-                <TableRow key="3">
-                  <TableCell>Jane Fisher</TableCell>
-                  <TableCell>Senior Developer</TableCell>
-                  <TableCell>Active</TableCell>
-                </TableRow>
-                <TableRow key="4">
-                  <TableCell>William Howard</TableCell>
-                  <TableCell>Community Manager</TableCell>
-                  <TableCell>Vacation</TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </AccordionItem>
-        </Accordion>
+
+      <div className="pt-10">
+        <div className="px-4 pb-4 mx-auto w-full max-w-[800px] items-center justify-around flex flex-col">
+          <h1 className="max-w-[700px] inline font-semibold text-2xl md:text-3xl text-left  w-full px-4 md:px-0">
+            Front-End
+          </h1>
+          <p className="leading-7 md:leading-9 px-auto mx-auto pt-2 text-left text-md sm:text-xl items-center justify-center max-w-[700px] ">
+            I built multiple full-stack application using JavaScript
+            meta-frameworks such as Next.js, and Nuxt.js, I am currently
+            expanding
+          </p>
+        </div>
+        <div className="pt-4 max-w-[700px] mx-auto">
+          <Accordion variant="splitted">
+            <AccordionItem key="1" aria-label="Skills" title="Skills">
+              <Table aria-label="Example static collection table">
+                <TableHeader>
+                  <TableColumn>NAME</TableColumn>
+                  <TableColumn>ROLE</TableColumn>
+                  <TableColumn>STATUS</TableColumn>
+                </TableHeader>
+                <TableBody>
+                  <TableRow key="1">
+                    <TableCell>Tony Reichert</TableCell>
+                    <TableCell>CEO</TableCell>
+                    <TableCell>Active</TableCell>
+                  </TableRow>
+                  <TableRow key="2">
+                    <TableCell>Zoey Lang</TableCell>
+                    <TableCell>Technical Lead</TableCell>
+                    <TableCell>Paused</TableCell>
+                  </TableRow>
+                  <TableRow key="3">
+                    <TableCell>Jane Fisher</TableCell>
+                    <TableCell>Senior Developer</TableCell>
+                    <TableCell>Active</TableCell>
+                  </TableRow>
+                  <TableRow key="4">
+                    <TableCell>William Howard</TableCell>
+                    <TableCell>Community Manager</TableCell>
+                    <TableCell>Vacation</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </AccordionItem>
+          </Accordion>
+        </div>
+      </div>
+      <div className="pt-10 pb-20">
+        <div className="px-4 pb-4 mx-auto w-full max-w-[800px] items-center justify-around flex flex-col">
+          <h1 className="max-w-[700px] inline font-semibold text-2xl md:text-3xl text-left  w-full px-4 md:px-0 ">
+            Front-End
+          </h1>
+          <p className="leading-7 md:leading-9 px-auto mx-auto pt-2 text-left text-md sm:text-xl items-center justify-center max-w-[700px] ">
+            I built multiple full-stack application using JavaScript
+            meta-frameworks such as Next.js, and Nuxt.js, I am currently
+            expanding
+          </p>
+        </div>
+        <div className="pt-4 max-w-[700px] mx-auto">
+          <Accordion variant="splitted">
+            <AccordionItem key="1" aria-label="Skills" title="Skills">
+              <Table aria-label="Example static collection table">
+                <TableHeader>
+                  <TableColumn>NAME</TableColumn>
+                  <TableColumn>ROLE</TableColumn>
+                  <TableColumn>STATUS</TableColumn>
+                </TableHeader>
+                <TableBody>
+                  <TableRow key="1">
+                    <TableCell>Tony Reichert</TableCell>
+                    <TableCell>CEO</TableCell>
+                    <TableCell>Active</TableCell>
+                  </TableRow>
+                  <TableRow key="2">
+                    <TableCell>Zoey Lang</TableCell>
+                    <TableCell>Technical Lead</TableCell>
+                    <TableCell>Paused</TableCell>
+                  </TableRow>
+                  <TableRow key="3">
+                    <TableCell>Jane Fisher</TableCell>
+                    <TableCell>Senior Developer</TableCell>
+                    <TableCell>Active</TableCell>
+                  </TableRow>
+                  <TableRow key="4">
+                    <TableCell>William Howard</TableCell>
+                    <TableCell>Community Manager</TableCell>
+                    <TableCell>Vacation</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </AccordionItem>
+          </Accordion>
+        </div>
       </div>
     </div>
   );
