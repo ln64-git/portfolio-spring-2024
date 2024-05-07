034d092a0e6db11165c98ee51d541de9bc665c34
Luke <lucian.io@outlook.com>
2024-05-07 18:23:10 -0400

    removed fade effect, more professional

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,14 +45,14 @@ export default function RootLayout({
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative min-h-screen">
             <AuroraBackground className="fixed h-screen inset-0 z-0">
-              <ScrollShadow hideScrollBar size={50}>
-                <div className="relative z-10 overflow-y-scroll">
-                  <Navbar />
-                  <main className="container mx-auto max-w-7xl flex-grow">
-                    {children}
-                  </main>
-                </div>
-              </ScrollShadow>
+              {/* <ScrollShadow hideScrollBar  size={50}> */}
+              <div className="relative z-10 overflow-y-scroll">
+                <Navbar />
+                <main className="container mx-auto max-w-7xl flex-grow">
+                  {children}
+                </main>
+              </div>
+              {/* </ScrollShadow> */}
             </AuroraBackground>
           </div>
         </Providers>
